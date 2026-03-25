aabbb39b8fa6512acbc16258aba9faf4b4c7039d
Julian Easterling <julian@julianscorner.com>
2026-03-25 09:44:04 -0400

    Updated build script to retain image version during build

--- build.cmd
+++ build.cmd
@@ -3,20 +3,18 @@ setlocal
 
 pushd %~dp0
 
-for /f "delims=" %%x in (version) do set VERSION=%%x
+for /f "delims=" %%x in (version) do set IMAGE_VERSION=%%x
 
-docker build  --progress plain --build-arg VERSION=%VERSION% -t dcjulian29/bind:%VERSION% .
+docker build --progress plain --build-arg VERSION=%IMAGE_VERSION% -t dcjulian29/bind:%IMAGE_VERSION% .
 
 if %errorlevel% neq 0 GOTO FINAL
 
-docker tag dcjulian29/bind:%VERSION% dcjulian29/bind:latest
+docker tag dcjulian29/bind:%IMAGE_VERSION% dcjulian29/bind:latest
 
 :FINAL
 
 popd
 
-set IMAGE_VERSION=%VERSION%
-
-goreleaser --snapshot --skip-publish --clean
+goreleaser --snapshot --clean
 
 endlocal
